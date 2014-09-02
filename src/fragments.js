dbfa65a5773f5635401b9b3356a6b3c947d96318
Erwan Loisant <eloisant@gmail.com>
2014-09-02 09:33:18 +0200

    Add h4 to h6

--- src/fragments.js
+++ src/fragments.js
@@ -753,6 +753,9 @@
                 "heading1": "h1",
                 "heading2": "h2",
                 "heading3": "h3",
+                "heading4": "h4",
+                "heading5": "h5",
+                "heading6": "h6",
                 "paragraph": "p"
             };
 
