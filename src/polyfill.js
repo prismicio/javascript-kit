ff4da7ad8d6c95905e26b22e81b1fa50e50d702a
Erwan Loisant <eloisant@gmail.com>
2014-11-28 14:47:34 +0100

    jshint fix

--- src/polyfill.js
+++ src/polyfill.js
@@ -11,7 +11,7 @@
                     throw TypeError('Argument must be an object');
                 }
                 Object.prototype = prototype;
-                var result = new Object();
+                var result = {};
                 Object.prototype = null;
                 return result;
             };
